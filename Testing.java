309a5e9a54d1cf529bdfe30d7c6b56d8b65eb01d
SatyaKadali2752 <74218860+SatyaKadali2752@users.noreply.github.com>
2020-11-12 18:24:17 +1100

    Add files via upload

---
+++ Testing.java
@@ -0,0 +1,20 @@
+package com.company.demo;
+
+import org.imgscalr.Scalr;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+class ImageEditor {
+
+    static BufferedImage brightenImage(BufferedImage image) {
+        return Scalr.apply(image, Scalr.OP_BRIGHTER);
+    }
+
+    static BufferedImage monochrome(BufferedImage image) {
+        BufferedImage newImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+        Graphics2D graphics = newImage.createGraphics();
+        graphics.drawImage(image, 0, 0, null);
+        return newImage;
+    }
+}
